e47e42cdc12b8436425f3b779b8084e162b2d2bf
Annapaola Santarsiero <annapaola@Eamonn-Maguires-MacBook-Pro.local>
2014-02-12 19:26:33 +0000

    Initial commit of Pizzari group wordpress theme

---
+++ footer.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #main -->
+<div class="clearfix"></div>
+
+<div id="footer">
+<div class="centralPanel">
+
+   <div id="footer_nav_wrapper">
+
+           <div class="col col_med">
+               <img src="/wp-content/themes/zoo-pizzari/img/ees-abstract.png" alt="Logo"/>
+           </div>
+
+           <div class="col col_med">
+               <h3>Related Links</h3>
+               <ul>
+                   <li><a href="http://www.zoo.ox.ac.uk" target="_blank">Zoology Department</a></li>
+                   <li><a href="http://www.ox.ac.uk" target="_blank">Oxford University</a></li>
+               </ul>
+           </div>
+
+   <div class="col col_small">
+               <h3>Funders</h3>
+               <ul>
+                   <li><a href="http://www.nerc.ac.uk/" target="_blank">NERC</a></li>
+               </ul>
+           </div>
+
+           <div class="col col_small">
+               <h3>Contact</h3>
+               <ul>
+                   <li><a href="https://www.twitter.com/eesgroup" target="_blank">@eesgroup</a></li>
+                   <li><a href="mailto:tommaso.pizzari@zoo.ox.ac.uk">Email</a></li>
+               </ul>
+           </div>
+
+           <div class="col col_small">
+               <h3>Admin</h3>
+               <ul>
+                   <li><a href="/login" target="_blank">Log in</a></li>
+               </ul>
+           </div>
+
+
+       </div>
+
+
+    <div id="footer_fineprint" align="center">
+        ©2014-Present Pizzari Group, University of Oxford, Designed by <a href="http://www.annapaolasantarsiero.co.uk" target="_blank"> Annapaola Santarsiero</a>.
+    </div>
+
+</div>
+</div>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
